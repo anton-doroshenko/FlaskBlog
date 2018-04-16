be71ffd869a2523754dbebeb385eb6d6d79f041b
Anton Doroshenko <anton.doroshenko1@gmail.com>
2018-04-16 23:16:10 +0300

    Change flask_blog.py

--- flask_blog.py
+++ flask_blog.py
@@ -60,7 +60,7 @@ def login():
         else:
             session["logged_in"] = True
             flash("You were logged in")
-            redirect(url_for("show_entries"))
+            return redirect(url_for("show_entries"))
     return render_template('login.html', error = error)
 
 @app.route("/logout")
